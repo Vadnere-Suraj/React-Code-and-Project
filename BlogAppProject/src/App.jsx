7dfd6e1bce163d2cd11544ddb7d1a4992506a7cf
Vadnere-Suraj <surajvadnere07@gmail.com>
2024-07-15 18:25:50 +0530

    Starting a Mega Project SOON

---
+++ BlogAppProject/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import conf from './conf/conf'
+
+function App() {
+ 
+  console.log(conf.collectionID)
+  return (
+
+    
+    <>
+      <h1>A Blog App</h1>
+    </>
+  )
+}
+
+export default App
